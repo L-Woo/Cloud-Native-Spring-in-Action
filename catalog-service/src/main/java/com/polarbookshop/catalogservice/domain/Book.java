29bcc68caec3232c39ed178887b9203b5cd06e29
ss6116 <ss6116@daum.net>
2025-11-01 22:41:57 +0900

    카탈로그 서비스 Book클래스  검증 코드 추가

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -11,14 +11,14 @@ public record Book(
     regexp = "^[0-9]{10}|[0-9]{13}$",
     message = "The ISBN format must be valid."
   )
-  @NotBlank(
+  @NotBlank( // 이 필드는 널 값이 되어서는 안되고 화이트스페이스가 아닌 문자를 최소 하나 이상 가지고 있어야 한다.
     message = "The book title must be defined."
   )
   String isbn,
-  @NotBlank(message = "The book title must bve defined.")
+  @NotBlank(message = "The book title must be defined.")
   String title,
 
-  @NotBlank(message = "The book author must bve defined.")
+  @NotBlank(message = "The book author must be defined.")
   String author,
   @NotNull(message = "The book price must be defined.")
   @Positive(message = "The book price must be greater than zero") // 이 필드는 널 값이 되어서는 안 되고 0보다 큰 값을 가져야 한다.
