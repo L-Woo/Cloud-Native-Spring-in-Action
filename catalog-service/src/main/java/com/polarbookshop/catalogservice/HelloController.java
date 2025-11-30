a3bd7dc1cabecc88bb61d2cfa775cff0b6935eed
ss6116 <ss6116@daum.net>
2025-11-30 21:10:08 +0900

    4.2 외부화된 구성: 하나의 빌드, 여러 설정

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/HelloController.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String getGreeting() {
         return "도서 카탈로그에 오신 것을 환영합니다!";
     }
---
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -0,0 +1,20 @@
+package com.polarbookshop.catalogservice;
+
+import com.polarbookshop.catalogservice.config.PolarProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    // 사용자 정의 속성 액세스를 위해 생성자 오토와이어링을 통해 주입된 빈
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return polarProperties.getGreeting();   // 설정 데이터 빈에서 가져온 환영 메시지를 사용한다.
+    }
+}
